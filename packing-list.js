989e75c3e8b2316e2eb46640d62350483dd6523d
duducesconetto <duducesconetto1@gmail.com>
2022-03-22 18:04:10 +0000

    kitchen stuff to pack

--- packing-list.js
+++ packing-list.js
@@ -1,9 +1,18 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
-  console.log(packingList[i]);
+console.log("Kitchen stuff to pack");
+
+let i = 0;
+
+while (i < pakingList.length) {
+  
+   console.log(pakingList[i]);
+  
+   i++; 
 }
+
+
+
 
 
 
